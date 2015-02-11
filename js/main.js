d8da2ff4a86bf675a25cc0bc8e48807816231175
Chris Dhanaraj <chris.dhanaraj@thomsonreuters.com>
2015-02-11 00:51:24 -0500

    styling updates, added scroll button

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@
 
   $('.header__text').fitText(0.88);
 
+
+  $('.scroll-down').on('click', function() {
+    var target = $('#content-start');
+
+    $('html,body').animate({
+      scrollTop: target.offset().top
+    }, 800);
+  });
 })(jQuery);
