5f6f6c7dc7d14fbbaf19330bb5b2116d1883be78
Chris Dhanaraj <chris.dhanaraj@thomsonreuters.com>
2015-02-10 21:16:05 -0500

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -31,12 +31,10 @@ gulp.task('styles', function() {
     .pipe(sass())
     .pipe(autoprefixer(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')))
     .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(notify({ message: 'SASS compiled'}));
+    .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('notify-js', function() {
-  notifier.notify({message: 'JS saved'});
   browserSync.reload();
 });
 
